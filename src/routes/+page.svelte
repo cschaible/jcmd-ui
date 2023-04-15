fdc1a23d70b05d9ff52ec0dc6f74d0e349ed3257
Christian Schaible <cschaible@users.noreply.github.com>
2023-04-15 13:10:24 +0200

    Add Tauri

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
+<script>
+    let ms = 1000
+    let counter = 0
+    const incr = () => (counter += 1)
+    const reset = () => (counter = 0)
+
+    let clear
+    $: {
+        clearInterval(clear)
+        clear = setInterval(incr, ms)
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>{counter}</p>
+<input type="number" bind:value={ms} step="500" min="0" max="5000"/>
+<button on:click="{reset}">Reset</button>
